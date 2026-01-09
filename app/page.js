643e161ad152a9a872daeb7b517726c052c6d102
Adarsh108-tech <adarsh16221@gmail.com>
2026-01-10 01:47:57 +0530

    added db url

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
-const API_URL = "http://localhost:5000";
+const API_URL = process.env.NEXT_PUBLIC_DATABASE_URL;
 
 export default function AuthForm() {
   const router = useRouter();
